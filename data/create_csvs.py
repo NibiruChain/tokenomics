80ee2596657577835b1dea8e93cdc48c5e49780a
Unique-Divine <realuniquedivine@gmail.com>
2022-05-30 22:40:09 -0500

    Run auto-styler

--- data/create_csvs.py
+++ data/create_csvs.py
@@ -3,16 +3,19 @@ import xlsx
 import csv
 import os
 
+
 def csv_from_excel():
     excel_file_paths: List[str] = [f for f in os.listdir() if ("xlsx" in f)]
     workbook = xlsx.Workbook(excel_file_paths[0])
-    sheet: xlsx.Sheet = [sheet for sheet in workbook 
-                         if ("token_distribution" in sheet.name)][0]
-        
-    with open('token_distribution.csv', 'w') as csv_file:
+    sheet: xlsx.Sheet = [
+        sheet for sheet in workbook if ("token_distribution" in sheet.name)
+    ][0]
+
+    with open("token_distribution.csv", "w") as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for row_num, cells in sheet.rows().items():
             writer.writerow([c.value for c in cells])
+
 
 # runs the csv_from_excel function:
 
